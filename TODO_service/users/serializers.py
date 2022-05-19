c1a119e409d43fd498cec958c650d16aef0a5600
NikitaKozlov <leito-ishim@mail.ru>
2022-05-19 15:37:01 +0500

    'test2'

--- TODO_service/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import User
-
-
-class UserModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'birthday_year')
